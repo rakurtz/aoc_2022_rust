d15d7fc5b00c682efcce9d41ff6ec1de53641505
martin <martin.kurtz@icloud.com>
2023-11-04 16:13:10 +0100

    Day 4 - Part 2 complete

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,51 @@
 use std::fs;
 
 fn pt1_calculate(input: String) -> usize {
+    // range is fully contained by the other
     let mut count_fully_containing = 0;
 
     for line in input.lines() {
-        let line_parts:Vec<&str> = line.split(',').collect();
-        let first_a_b:Vec<&str> = line_parts[0].split('-').collect();
-        let second_a_b:Vec<&str> = line_parts[1].split('-').collect();
-        if second_a_b[0].parse::<usize>().unwrap() >= first_a_b[0].parse().unwrap() &&
-            second_a_b[1].parse::<usize>().unwrap() <= first_a_b[1].parse().unwrap() {
-                count_fully_containing += 1;
-            } else if second_a_b[0].parse::<usize>().unwrap() <= first_a_b[0].parse().unwrap() &&
-            second_a_b[1].parse::<usize>().unwrap() >= first_a_b[1].parse().unwrap() {
-                count_fully_containing += 1;
-            }
+        let line_parts: Vec<&str> = line.split(',').collect();
+        let first_a_b: Vec<&str> = line_parts[0].split('-').collect();
+        let second_a_b: Vec<&str> = line_parts[1].split('-').collect();
+        if second_a_b[0].parse::<usize>().unwrap() >= first_a_b[0].parse().unwrap()
+            && second_a_b[1].parse::<usize>().unwrap() <= first_a_b[1].parse().unwrap()
+        {
+            count_fully_containing += 1;
+        } else if second_a_b[0].parse::<usize>().unwrap() <= first_a_b[0].parse().unwrap()
+            && second_a_b[1].parse::<usize>().unwrap() >= first_a_b[1].parse().unwrap()
+        {
+            count_fully_containing += 1;
+        }
     }
     count_fully_containing
+}
+
+fn pt2_calculate(input: String) -> usize {
+    // ranges overlap
+    let mut count_overlapping = 0;
+
+    for line in input.lines() {
+        let line_parts: Vec<&str> = line.split(',').collect();
+        let first_a_b: Vec<&str> = line_parts[0].split('-').collect();
+        let second_a_b: Vec<&str> = line_parts[1].split('-').collect();
+        if (first_a_b[0].parse::<usize>().unwrap()..=first_a_b[1].parse::<usize>().unwrap())
+            .collect::<Vec<usize>>()
+            .contains(&second_a_b[0].parse::<usize>().unwrap())
+            || (first_a_b[0].parse::<usize>().unwrap()..=first_a_b[1].parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+                .contains(&second_a_b[1].parse::<usize>().unwrap())
+            || (second_a_b[0].parse::<usize>().unwrap()..=second_a_b[1].parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+                .contains(&first_a_b[0].parse::<usize>().unwrap())
+            || (second_a_b[0].parse::<usize>().unwrap()..=second_a_b[1].parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+                .contains(&first_a_b[1].parse::<usize>().unwrap())
+        {
+            count_overlapping += 1;
+        }
+    }
+    count_overlapping
 }
 
 fn main() {
@@ -26,7 +56,7 @@ fn main() {
     let input = fs::read_to_string("puzzle_inputs/input.txt").expect("Could not read input file");
 
     println!("Day 4, part 1 - {}", pt1_calculate(input.clone()));
-    // println!("Day 4, part 2 - {}", pt2_calculate(input.clone()));
+    println!("Day 4, part 2 - {}", pt2_calculate(input.clone()));
 }
 
 #[cfg(test)]
@@ -45,16 +75,16 @@ mod tests {
 
         assert_eq!(2, pt1_calculate(input));
     }
-//     #[test]
-//     fn sample_input_for_pt2() {
-//         let input = "vJrwpWtwJgWrhcsFMMfFFhFp
-// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-// PmmdzqPrVvPwwTWBwg
-// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-// ttgJtRGJQctTZtZT
-// CrZsJsPPZsGzwwsLwLmpwMDw"
-//             .to_string();
+    #[test]
+    fn sample_input_for_pt2() {
+        let input = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"
+            .to_string();
 
-//         assert_eq!(70, pt2_calculate(input));
-//     }
+        assert_eq!(4, pt2_calculate(input));
+    }
 }
