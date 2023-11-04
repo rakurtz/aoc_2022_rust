7cbd24a004c9f09e23377379dba4be752f7bec45
martin <martin.kurtz@icloud.com>
2023-11-04 15:52:04 +0100

    Day 4 Part 1 finished

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,32 @@
 // Advent of Code 2022 in Rust
-// Day 3
+// Day 4
 
 use std::fs;
 
-static ASCII_LOWER: [char; 26] = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z',
-];
-
-fn duplicate_item(line: &str) -> Result<char, String> {
-    let compartment_a = &line[0..line.len() / 2];
-    let compartnemt_b = &line[line.len() / 2..];
-    for c in compartment_a.chars() {
-        if compartnemt_b.contains(c) {
-            return Ok(c);
-        }
-    }
-    Err("no duplicate result found".to_string())
-}
-
-fn common_in_three_lines(three_lines: Vec<&str>) -> Result<char, String> {
-    for c in three_lines.get(0).unwrap().chars() {
-        if three_lines.get(1).unwrap().contains(c) && three_lines.get(2).unwrap().contains(c) {
-            return Ok(c);
-        }
-    }
-    Err("no common item found within three lines".to_string())
-}
-
-fn priority_of_char(c: char) -> usize {
-    // returns the priority of a given char c by finding its position in the alphabet (static ASCII_LOWER)
-    // since the priority for upper case charecters ist the same as for lower case character plus 26
-    // the .position() is only run on to_lowercase()ed characters
-
-    if c.is_lowercase() {
-        let index = ASCII_LOWER.iter().position(|&r| r == c).unwrap();
-        return index + 1;
-    } else {
-        let index = ASCII_LOWER
-            .iter()
-            .position(|&r| r == c.to_lowercase().next().unwrap())
-            .unwrap();
-        return index + 1 + 26;
-    }
-}
-
 fn pt1_calculate(input: String) -> usize {
-    let mut priority_sum = 0;
+    let mut count_fully_containing = 0;
 
     for line in input.lines() {
-        let c = duplicate_item(line).unwrap();
-        priority_sum += priority_of_char(c)
+        let line_parts:Vec<&str> = line.split(',').collect();
+        let first_a_b:Vec<&str> = line_parts[0].split('-').collect();
+        let second_a_b:Vec<&str> = line_parts[1].split('-').collect();
+        if second_a_b[0].parse::<usize>().unwrap() >= first_a_b[0].parse().unwrap() &&
+            second_a_b[1].parse::<usize>().unwrap() <= first_a_b[1].parse().unwrap() {
+                count_fully_containing += 1;
+            } else if second_a_b[0].parse::<usize>().unwrap() <= first_a_b[0].parse().unwrap() &&
+            second_a_b[1].parse::<usize>().unwrap() >= first_a_b[1].parse().unwrap() {
+                count_fully_containing += 1;
+            }
     }
-
-    priority_sum
-}
-
-fn pt2_calculate(input: String) -> usize {
-    // generates a vectors containging three lines and calls common_in_three_lines()
-    // then sums up all their priorities
-
-    // question: how to not allocate new vectors here but send references (slices) ?
-
-    let mut priority_sum = 0;
-    let vec_of_lines: Vec<&str> = input.lines().collect();
-
-    for x in (0..vec_of_lines.len()).step_by(3) {
-        let mut vec_of_three = Vec::new();
-        vec_of_three.push(vec_of_lines[x]);
-        vec_of_three.push(vec_of_lines[x + 1]);
-        vec_of_three.push(vec_of_lines[x + 2]);
-
-        let c = common_in_three_lines(vec_of_three).unwrap();
-        priority_sum += priority_of_char(c);
-    }
-    priority_sum
+    count_fully_containing
 }
 
 fn main() {
     // read file to string
     let input = fs::read_to_string("puzzle_inputs/input.txt").expect("Could not read input file");
 
-    println!("Day 3, part 1 - {}", pt1_calculate(input.clone()));
-    println!("Day 3, part 2 - {}", pt2_calculate(input.clone()));
+    println!("Day 4, part 1 - {}", pt1_calculate(input.clone()));
+    // println!("Day 4, part 2 - {}", pt2_calculate(input.clone()));
 }
 
 #[cfg(test)]
@@ -91,26 +35,26 @@ mod tests {
 
     #[test]
     fn sample_input_for_pt1() {
-        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw"
+        let input = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"
             .to_string();
 
-        assert_eq!(157, pt1_calculate(input));
+        assert_eq!(2, pt1_calculate(input));
     }
-    #[test]
-    fn sample_input_for_pt2() {
-        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw"
-            .to_string();
+//     #[test]
+//     fn sample_input_for_pt2() {
+//         let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+// PmmdzqPrVvPwwTWBwg
+// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+// ttgJtRGJQctTZtZT
+// CrZsJsPPZsGzwwsLwLmpwMDw"
+//             .to_string();
 
-        assert_eq!(70, pt2_calculate(input));
-    }
+//         assert_eq!(70, pt2_calculate(input));
+//     }
 }
